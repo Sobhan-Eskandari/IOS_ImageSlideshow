4e154e9390eebf078e650a6963b84bc0a9663d2f
Sobhan-Eskandari <arash1@mail.usf.edu>
2016-11-25 21:33:34 +0330

    Added Loading From Network with Alamofire

--- Slideshow/ViewController.swift
+++ Slideshow/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
 
     @IBOutlet var slideshow: ImageSlideshow!
     
+    // Loading Images Locally
     let localSource = [ImageSource(imageString: "img1")!, ImageSource(imageString: "img2")!, ImageSource(imageString: "img3")!, ImageSource(imageString: "img4")!]
 
+    //Loading Images From Network
+     let alamofireSource = [AlamofireSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080")!, AlamofireSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080")!, AlamofireSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")!]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +30,7 @@ class ViewController: UIViewController {
         slideshow.contentScaleMode = UIViewContentMode.scaleAspectFill
         
         // try out other sources such as `afNetworkingSource`, `alamofireSource` or `sdWebImageSource` or `kingfisherSource`
-        slideshow.setImageInputs(localSource)
+        slideshow.setImageInputs(alamofireSource)
         
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.didTap))
         slideshow.addGestureRecognizer(recognizer)
